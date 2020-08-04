2d03289c74fd7c435ccb249bd323c503df7ed352
calvinliu0623 <65231734+calvinliu0623@users.noreply.github.com>
2020-08-04 20:20:38 +0800

    Add files via upload

---
+++ m.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  4 19:59:39 2020
+
+@author: CALVIN
+"""
+
+
+def MultiplicationTable(a,b):
+    for j in range(1,10):
+        print(str(i)+'x'+str(j)+'='+str(i*))
+    
