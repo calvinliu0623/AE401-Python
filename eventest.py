3331de3140f162053b54d2e15f29e69921bd408a
calvinliu0623 <65231734+calvinliu0623@users.noreply.github.com>
2020-08-04 20:20:25 +0800

    Add files via upload

---
+++ eventest.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  4 19:40:18 2020
+
+@author: CALVIN
+"""
+def EvenTest():
+    d=int(input("input something......"))
+    if(d%2==1):
+        print('基數')
+    else:
+        print('偶數')
+EvenTest()
+    
+    
